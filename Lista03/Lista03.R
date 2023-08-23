410a1a7bd8e6e7ded1ffea009083b893fa0be349
Nathaly Izawa <128828789+NLissaSI@users.noreply.github.com>
2023-08-23 19:21:01 -0300

    Update and rename Lista03.R to Lista03/Lista03.R

--- Lista03/Lista03.R
+++ Lista03/Lista03.R
@@ -8,10 +8,8 @@ fit = lm(educ ~ motheduc + fatheduc + abil + I(abil^2), htv)
 fit$coefficients #coef(lm(educ ~ motheduc + fatheduc + abil + I(abil^2), htv))
 # se abil aumenta 1, educ aumenta 0.4015, mantendo os outros fatores fixos
 
-# Para uma analise correta, devemos derivar o modelo com relacao ao abil
-# assim temos que
+# Para uma analise correta, devemos derivar o modelo com relacao ao abil assim temos que
 fit$coefficients[4] + 2 * fit$coefficients[5]
 
 # Ponto de inflexao
 - fit$coefficients[5] / (2 * fit$coefficients[4])
-
