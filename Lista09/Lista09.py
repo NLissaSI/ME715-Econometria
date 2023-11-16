0ebc8132816a318812c7f5982b1b24e57a31bcd8
Nathaly Izawa <128828789+NLissaSI@users.noreply.github.com>
2023-11-15 21:31:26 -0300

    Update Lista09.py

--- Lista09/Lista09.py
+++ Lista09/Lista09.py
@@ -1,28 +1,29 @@
+# Questao 3)
 import pandas as pd
 
-# Instalando o pacote `pyreadstat`
+## Instalando o pacote `pyreadstat`
 !pip install pyreadstat #--quiet
 # Pacote para ler .dta
 import pyreadstat
 
-# Instalando o pacote `linearmodels`
+## Instalando o pacote `linearmodels`
 !pip install linearmodels #--quiet
 # Funcao para o modelo SUR
 from linearmodels.system import SUR
 
-# Leitura dos dados
+## Leitura dos dados
 df, meta = pyreadstat.read_dta('/content/hsb2.dta')
 df
 
-# Formula
+## Formula
 formula = {"eq01": "read ~ 1 + female + pd.to_numeric(ses) + socst", 
            "eq02": "math ~ 1 + female + pd.to_numeric(ses) + science"}
 
-# Metodo SUR
+## Metodo SUR
 mod = SUR.from_formula(formula, df)
 
-## Generalizes Least Squares (GLS)
+### Generalizes Least Squares (GLS)
 mod.fit()
 
-## Ordinary Least Squares (OLS)
+### Ordinary Least Squares (OLS)
 mod.fit(method="ols")
